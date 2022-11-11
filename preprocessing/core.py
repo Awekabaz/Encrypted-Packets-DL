0f1d827f7d47c402db66d52891f4ae1ab22ca2ee
Alisher Bazarbay <aweka@Alishers-MacBook-Pro.local>
2022-11-11 15:00:25 +0800

    rename

--- preprocessing/core.py
+++ preprocessing/core.py
@@ -1,7 +1,12 @@
 from pathlib import Path
 
-from transform import MyPacket
+from mypacket import MyPacket
 from utils import read_pcap
 
 
 def preocess_pcap_file(path, path_output: Path = None, batch_size: int = 10000):
+    if Path(str(path_output.absolute()) + '_SUCCESS').exists():
+        print(path_output, 'Done')
+        return
+
+    
